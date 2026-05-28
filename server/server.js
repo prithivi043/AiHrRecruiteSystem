54b5d34f35cca1b8bff95eaa8b56488b38fcd031
prithivi043 <prithivigithub043@gmail.com>
2026-05-28 17:50:58 +0530

    vercel added

--- server/server.js
+++ server/server.js
@@ -38,7 +38,35 @@ const app = express();
 // MIDDLEWARE
 // ========================================
 
-app.use(cors());
+app.use(
+
+  cors({
+
+    origin: [
+
+      "https://ai-hr-recruite-system-ctlf.vercel.app",
+    ],
+
+    credentials: true,
+
+    methods: [
+
+      "GET",
+      "POST",
+      "PUT",
+      "DELETE",
+      "OPTIONS",
+    ],
+
+    allowedHeaders: [
+
+      "Content-Type",
+      "Authorization",
+    ],
+  })
+);
+
+
 
 app.use(express.json());
 
